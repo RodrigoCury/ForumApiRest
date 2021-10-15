c2dec47bcf0f8ca3e1457b2457b4510dc1880e82
= <=>
2021-10-15 19:40:25 -0300

    Filter using JPQL

--- forum/src/main/java/br/dev/rodrigocury/forum/repositories/TopicoRepository.java
+++ forum/src/main/java/br/dev/rodrigocury/forum/repositories/TopicoRepository.java
@@ -1,7 +1,14 @@
 package br.dev.rodrigocury.forum.repositories;
 
 import br.dev.rodrigocury.forum.models.Topico;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TopicoRepository extends CrudRepository<Topico, Long> {
+
+  @Query("select t from Topico t join t.curso curso where curso.nome = :nomeDoCurso")
+  List<Topico> findAllByNomeDoCurso(@Param("nomeDoCurso") String nomeDoCurso);
 }
