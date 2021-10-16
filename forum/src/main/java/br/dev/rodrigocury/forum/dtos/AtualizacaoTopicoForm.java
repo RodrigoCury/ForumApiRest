6b4031b45bb97fc96371249a85fcce54b1fd25e2
= <=>
2021-10-16 16:12:57 -0300

    Create AtualizacaoTopicoForm.java

---
+++ forum/src/main/java/br/dev/rodrigocury/forum/dtos/AtualizacaoTopicoForm.java
@@ -0,0 +1,45 @@
+package br.dev.rodrigocury.forum.dtos;
+
+import br.dev.rodrigocury.forum.models.Topico;
+import br.dev.rodrigocury.forum.repositories.TopicoRepository;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+public class AtualizacaoTopicoForm {
+  @NotNull @NotEmpty @Length(min = 5)
+  private String titulo;
+  @NotNull @NotEmpty @Length(min = 5)
+  private String mensagem;
+
+  public AtualizacaoTopicoForm() {
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getMensagem() {
+    return mensagem;
+  }
+
+  public void setMensagem(String mensagem) {
+    this.mensagem = mensagem;
+  }
+
+  public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+    Optional<Topico> topico = topicoRepository.findById(id);
+    if (topico.isEmpty()){
+      return null;
+    }
+    topico.get().setMensagem(mensagem);
+    topico.get().setTitulo(titulo);
+    return topico.get();
+  }
+}
