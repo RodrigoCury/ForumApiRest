ba13f0105a6a8455aeb920ad536041212668c0da
= <=>
2021-10-17 20:18:05 -0300

    Using Stateless Session Cretion Policy

--- forum/src/main/java/br/dev/rodrigocury/forum/config/security/SecurityConfigs.java
+++ forum/src/main/java/br/dev/rodrigocury/forum/config/security/SecurityConfigs.java
@@ -1,27 +1,50 @@
 package br.dev.rodrigocury.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigs extends WebSecurityConfigurerAdapter {
 
+  AutenticacaoService autenticacaoService;
+
+  @Autowired
+  public void setAutenticacaoService(AutenticacaoService autenticacaoService) {
+    this.autenticacaoService = autenticacaoService;
+  }
+
+  @Bean
+  @Override
+  protected AuthenticationManager authenticationManager() throws Exception {
+    return super.authenticationManager();
+  }
+
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    super.configure(auth);
+    auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
         .antMatchers(HttpMethod.GET, "/api/topicos").permitAll()
-        .antMatchers(HttpMethod.GET, "/api/topicos/*").permitAll();
+        .antMatchers(HttpMethod.GET, "/api/topicos/*").permitAll()
+        .antMatchers(HttpMethod.POST, "/auth").permitAll()
+        .anyRequest().authenticated()
+        .and().csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        ;
   }
 
   @Override
