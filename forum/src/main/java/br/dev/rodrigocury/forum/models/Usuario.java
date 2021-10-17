6df44a37c446bf09898649838f72a875dc673a8f
= <=>
2021-10-17 20:20:39 -0300

    IMplemented User Detail

--- forum/src/main/java/br/dev/rodrigocury/forum/models/Usuario.java
+++ forum/src/main/java/br/dev/rodrigocury/forum/models/Usuario.java
@@ -1,11 +1,15 @@
 package br.dev.rodrigocury.forum.models;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,6 +17,9 @@ public class Usuario {
   private String nome;
   private String email;
   private String senha;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  private List<Perfil> perfis;
 
   @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "autor")
   private List<Topico> topicos = new ArrayList<>();
@@ -74,4 +81,38 @@ public class Usuario {
     this.senha = senha;
   }
 
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return this.perfis;
+  }
+
+  @Override
+  public String getPassword() {
+    return this.senha;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
